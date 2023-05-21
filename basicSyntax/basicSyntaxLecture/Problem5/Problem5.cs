cfb13d7348eadaf703e74ffc39162f8ee438515f
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-05-21 12:45:31 +0300

    -

---
+++ basicSyntax/basicSyntaxLecture/Problem5/Problem5.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthNumber = int.Parse(Console.ReadLine());
+            //
+            switch (monthNumber)
+            {
+                case 1:
+                    Console.WriteLine("January");
+                    break;
+                case 2:
+                    Console.WriteLine("January");
+                    break;
+                case 3:
+                    Console.WriteLine("January");
+                    break;
+                case 4:
+                    Console.WriteLine("January");
+                    break;
+                case 5:
+                    Console.WriteLine("January");
+                    break;
+                case 6:
+                    Console.WriteLine("January");
+                    break;
+                case 7:
+                    Console.WriteLine("January");
+                    break;
+                case 8:
+                    Console.WriteLine("January");
+                    break;
+                case 9:
+                    Console.WriteLine("January");
+                    break;
+                case 10:
+                    Console.WriteLine("January");
+                    break;
+                case 11:
+                    Console.WriteLine("January");
+                    break;
+                case 12:
+                    Console.WriteLine("January");
+                    break;
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+
+
+            }
+        }
+    }
+}
