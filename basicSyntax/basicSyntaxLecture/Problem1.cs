1c0e9555f5c45d866d8620ce2dfbb7da5295b241
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-05-21 11:57:36 +0300

    -

--- basicSyntax/basicSyntaxLecture/Problem1.cs
+++ basicSyntax/basicSyntaxLecture/Problem1.cs
@@ -10,6 +10,12 @@ namespace basicSyntaxLecture
     {
         static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            float avarageAge = float.Parse(Console.ReadLine());
+            //
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {avarageAge:f2}");
+
         }
     }
 }
---
+++ basicSyntax/basicSyntaxLecture/Problem2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicSyntaxLecture
+{
+    internal class Problem2
+    {
+        static void Main(string[] args)
+        {
+            float grade = float.Parse(Console.ReadLine());
+            //
+            if (grade >= 3.00)
+            {
+                Console.WriteLine($"Passed!");
+            }
+        }
+    }
+}
