80879886d3908a9ec26910af3fa621b128556398
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-05-21 13:52:10 +0300

    -

--- basicSyntax/basicSyntaxLecture/Problem5/Problem5.cs
+++ basicSyntax/basicSyntaxLecture/Problem5/Problem5.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Problem5
 {
-    internal class Program
+    internal class Problem5
     {
         static void Main(string[] args)
         {
@@ -18,37 +18,37 @@ namespace Problem5
                     Console.WriteLine("January");
                     break;
                 case 2:
-                    Console.WriteLine("January");
+                    Console.WriteLine("February");
                     break;
                 case 3:
-                    Console.WriteLine("January");
+                    Console.WriteLine("March");
                     break;
                 case 4:
-                    Console.WriteLine("January");
+                    Console.WriteLine("April");
                     break;
                 case 5:
-                    Console.WriteLine("January");
+                    Console.WriteLine("May");
                     break;
                 case 6:
-                    Console.WriteLine("January");
+                    Console.WriteLine("June");
                     break;
                 case 7:
-                    Console.WriteLine("January");
+                    Console.WriteLine("July");
                     break;
                 case 8:
-                    Console.WriteLine("January");
+                    Console.WriteLine("August");
                     break;
                 case 9:
-                    Console.WriteLine("January");
+                    Console.WriteLine("September");
                     break;
                 case 10:
-                    Console.WriteLine("January");
+                    Console.WriteLine("October");
                     break;
                 case 11:
-                    Console.WriteLine("January");
+                    Console.WriteLine("November");
                     break;
                 case 12:
-                    Console.WriteLine("January");
+                    Console.WriteLine("December");
                     break;
                 default:
                     Console.WriteLine("Error!");
---
+++ basicSyntax/basicSyntaxLecture/Problem6/Problem6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem5
+{
+    internal class Problem5
+    {
+        static void Main(string[] args)
+        {
+            string monthNumber = Console.ReadLine();
+            //
+            if (monthNumber == "USA" || monthNumber == "Englang")
+            {
+                Console.WriteLine("English");
+            }
+            else if (monthNumber == "Spain" || monthNumber == "Argentina" || monthNumber == "Mexico")
+            {
+                    Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+
+        }
+    }
+}
