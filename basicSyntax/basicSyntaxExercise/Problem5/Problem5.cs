ff14221dafe335974af782a1f7e866f90bb6c587
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-05-25 00:29:18 +0300

    -

--- basicSyntax/basicSyntaxExercise/Problem5/Problem5.cs
+++ basicSyntax/basicSyntaxExercise/Problem5/Problem5.cs
@@ -10,6 +10,39 @@ namespace Problem5
     {
         static void Main(string[] args)
         {
+            var username = Console.ReadLine();
+            var passwordInput = Console.ReadLine();
+            char[] password = username.ToCharArray();
+            Array.Reverse(password);
+            string passwordValid = new string(password);
+            //
+            var isValid = false;
+            var counter = 1;
+            //
+            while (isValid != true)
+            {
+                if (passwordInput == passwordValid)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else
+                {
+                    if (counter >= 4)
+                    {
+                        isValid = true;
+                        break;
+                    }
+                    Console.WriteLine($"Incorrect password. Try again.");
+                    counter++;
+                }
+                passwordInput = Console.ReadLine();
+            }
+            if (isValid == true)
+            {
+                Console.WriteLine($"User {username} blocked!");
+            }
+
         }
     }
 }
---
+++ basicSyntax/basicSyntaxExercise/Problem6/Problem6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6
+{
+    internal class Problem6
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            string numString = num.ToString();
+            var factorialSumWhole = 0;
+            //
+            foreach (char digitChar in numString)
+            {
+                int digit = int.Parse(digitChar.ToString());
+                if (digit == 0)
+                {
+                    //if digit is 0 - error! by default 0! = 1!!!
+                    digit = 1;
+                }
+                int factorialSum = digit;
+                for (int i = digit-1; i >= 1; i--)
+                {
+                    factorialSum *= i;
+                }
+                factorialSumWhole += factorialSum;
+            }
+            if (factorialSumWhole == num)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
