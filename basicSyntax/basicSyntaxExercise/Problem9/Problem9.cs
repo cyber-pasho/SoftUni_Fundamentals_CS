822bca6bf6f521ee62d2f0499b985b1c2c34deb3
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-05-27 11:58:32 +0300

    `

---
+++ basicSyntax/basicSyntaxExercise/Problem9/Problem9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem9
+{
+    internal class Problem9
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
