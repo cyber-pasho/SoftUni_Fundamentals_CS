707197340f2df13945d484ce93c52c0cbd252979
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-05-27 23:35:46 +0300

    `

---
+++ dataTypesAndVariables/Problem5/Problem5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4
+{
+    internal class Problem4
+    {
+        static void Main(string[] args)
+        {
+            int code1 = int.Parse(Console.ReadLine());
+            char asciiCode1 = (char)code1;
+            int code2 = int.Parse(Console.ReadLine());
+            char asciiCode2 = (char)code2;
+            Console.Write($"{asciiCode1} ");
+            for (int i = asciiCode1+1; i < asciiCode2; i++)
+            {
+                char iAscii = (char)i;
+                Console.Write($"{iAscii} ");
+            }
+            Console.Write($"{asciiCode2}");
+        }
+    }
+}
---
+++ dataTypesAndVariables/Problem6/Problem6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6
+{
+    internal class Problem6
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            char letter = (char)('a' + num-1);
+            //
+            for (int i = 0; i < num; i++)
+            {
+                for (int k = 0; k < num; k++)
+                {
+                    for (int z = 0; z < num; z++)
+                    {
+                        char firstChar = (char)('a' + i);
+                        char secondChar = (char)('a' + k);
+                        char thirdChar = (char)('a' + z);
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+        }
+    }
+}
