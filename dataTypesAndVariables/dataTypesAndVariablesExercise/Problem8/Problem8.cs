c9c490a2385ea8e84462d9b66c78714c077ca0da
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-05-28 16:20:16 +0300

    8/11

--- dataTypesAndVariables/dataTypesAndVariablesExercise/Problem8/Problem8.cs
+++ dataTypesAndVariables/dataTypesAndVariablesExercise/Problem8/Problem8.cs
@@ -10,7 +10,25 @@ namespace Problem8
     {
         static void Main(string[] args)
         {
-
+            int numKegs = int.Parse(Console.ReadLine());
+            double[] volumes = new double[numKegs];
+            string[] models= new string[numKegs];
+            double largestVolume = double.MinValue;
+            int largestIndex = -1;
+            //
+            for (int i = 0; i < numKegs; i++)
+            {
+                models[i] = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                volumes[i] = Math.PI * (radius * radius) * height;
+                if (volumes[i] > largestVolume)
+                {
+                    largestVolume = volumes[i];
+                    largestIndex = i;
+                }
+            }
+            Console.WriteLine(models[largestIndex]);
         }
     }
 }
