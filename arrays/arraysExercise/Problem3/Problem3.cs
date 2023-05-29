7176663e200c7c58832f3645daeb6c35b97d8593
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-05-30 01:35:58 +0300

    `

---
+++ arrays/arraysExercise/Problem3/Problem3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    internal class Problem3
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            bool isFirstSelected = true;
+            string[] arrays1= new string[num]; //cherno
+            string[] arrays2= new string[num]; //cherveno
+            //
+            for (int i = 0; i < num; i++)
+            {
+                string arraysInput = Console.ReadLine();
+                string[] arraysInputSplit = arraysInput.Split();
+                if (isFirstSelected)
+                {
+                    arrays1[i] = arraysInputSplit[0];
+                    arrays2[i] = arraysInputSplit[1];
+                }
+                else
+                {
+                    arrays1[i] = arraysInputSplit[1];
+                    arrays2[i] = arraysInputSplit[0];
+                }
+                isFirstSelected = !isFirstSelected;
+            }
+            Console.WriteLine(string.Join(" ", arrays1));
+            Console.WriteLine(string.Join(" ", arrays2));
+        }
+    }
+}
---
+++ arrays/arraysExercise/Problem4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+            //
+            int rule = int.Parse(Console.ReadLine());
+            //
+            if (rule < items.Length)
+            {
+                rule = rule;
+            }
+            else
+            {
+                rule %= items.Length;
+            }
+            string[] itemsMoved= new string[rule];
+            Array.Copy(items, itemsMoved, rule);
+            Array.Copy(items, rule, items, 0, items.Length - rule);
+            Array.Copy(itemsMoved, 0, items, items.Length - rule, rule);
+
+            //
+            Console.WriteLine(string.Join(" ", items));
+        }
+    }
+}
