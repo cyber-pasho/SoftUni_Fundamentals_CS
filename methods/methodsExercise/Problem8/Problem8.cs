3fdc8193b0fc0f682aa958601430682f2e42bf06
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-06-07 18:24:21 +0300

    `

---
+++ methods/methodsExercise/Problem8/Problem8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicSyntaxExercise
+{
+    internal class Problem8
+    {
+        static void Main(string[] args)
+        {
+            long a = long.Parse(Console.ReadLine());
+            long b = long.Parse(Console.ReadLine());
+            double result = (double)FactorelA(a) / FactorelA(b);
+            Console.WriteLine($"{result:f2}");
+        }
+        static long FactorelA(long a)
+        {
+            long factorelA = a;
+            if (factorelA == 0) 
+            {
+                factorelA = 1;
+            }
+            for (int i = (int)factorelA - 1; i >= 1; i--)
+            {
+                if (i == 0)
+                {
+                    i = 1;
+                }
+                factorelA *= i;
+            }
+            return factorelA;
+        }
+       
+    }
+}
