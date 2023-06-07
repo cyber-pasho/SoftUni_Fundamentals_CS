d7ad62db6eb3b1767048b351f82902708f78c8c4
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-06-07 22:28:49 +0300

    `

---
+++ methods/methodsExercise/Problem10/Problem10.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem10
+{
+    internal class Problem10
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int currentNum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                currentNum = i;
+                if (DivisibleByEight(currentNum) && HoldsOddDigit(currentNum))
+                {
+                    Console.WriteLine($"{currentNum}");
+                }
+            }
+        }
+        static bool DivisibleByEight(int currentNum)
+        {
+            string currentNumString = currentNum.ToString();
+            int currentNumSum = 0;
+            for (int i = 0; i < currentNumString.Length; i++)
+            {
+                currentNumSum += (int)currentNumString[i]; 
+            }
+            if (currentNumSum % 8 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool HoldsOddDigit(int currentNum)
+        {
+            string currentNumString = currentNum.ToString();
+
+            for (int i = 0; i < currentNumString.Length; i++)
+            {
+                int currentNumInt = (int)currentNumString[i];
+                if (currentNumInt % 2 == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ methods/methodsExercise/Problem9/Problem9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem9
+{
+    internal class Problem9
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (BackwardsString(input) == input)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                input = Console.ReadLine();
+            }
+
+        }
+        static string BackwardsString(string input)
+        {
+            string palindrome = string.Empty;
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                palindrome += input[i];
+            }
+            return palindrome;
+        }
+    }
+}
