cf95cc59f071d892a1df984064069aeebbdca71f
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-06-13 23:37:57 +0300

    `

--- lists/listsExercise/Problem1/Problem1.cs
+++ lists/listsExercise/Problem1/Problem1.cs
@@ -10,7 +10,39 @@ namespace listsExercise
     {
         static void Main(string[] args)
         {
+            List<int> train = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList(); 
+            int capacity = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            //
+            while (input != "end")
+            {
+                List<string> inputs = input
+                .Split(' ')
+                .ToList();
+                if (inputs.Count > 1)
+                {
+                    int inputInt = int.Parse(inputs[1]);
+                    train.Add(inputInt);
+                }
+                else
+                {
+                    for (int i = 0; i < train.Count; i++)
+                    {
+                        int inputInt = int.Parse(inputs[0]);
+                        if (train[i] + inputInt <= capacity)
+                        {
+                            train[i] += inputInt;
+                            break;
+                        }
+                    }
+                }
+                input = Console.ReadLine();
 
+            }
+            Console.WriteLine(string.Join(" ", train));
         }
     }
 }
---
+++ lists/listsExercise/Problem2/Problem2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    internal class Problem2
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            string input = Console.ReadLine();
+            //
+            while (input != "end")
+            {
+                List<string> inputs = input
+                .Split(' ')
+                .ToList();
+                if (inputs.Count == 2)
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] == int.Parse(inputs[1]))
+                        {
+                            list.Remove(int.Parse(inputs[1]));
+                            i -= 1;
+                        }
+                    }
+                }
+                else
+                {
+                    list.Insert(int.Parse(inputs[2]), int.Parse(inputs[1]));
+                }
+                input = Console.ReadLine(); 
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
