e4b1e788132ac1590cb5b24088508cee2a12292e
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-06-15 01:27:59 +0300

    `

---
+++ lists/listsExercise/Problem4/Problem4.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix4
+{
+    internal class Problem4
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            string input = Console.ReadLine();
+            //
+            while (input != "End")
+            {
+                List<string> inputs = input
+                .Split(' ')
+                .ToList();
+                //
+                if (inputs[0] == "Add")
+                {
+                    int num = int.Parse(inputs[1]);
+                    list.Add(num);
+                }
+                else if (inputs[0] == "Insert")
+                {
+                    int num1 = int.Parse(inputs[1]);
+                    int num2 = int.Parse(inputs[2]);
+                    if (num2 > list.Count-1)
+                    {
+                        Console.WriteLine("Invalid index");
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    list.Insert(num2, num1);
+                }
+                else if (inputs[0] == "Remove")
+                {
+                    int num = int.Parse(inputs[1]);
+                    if (num > list.Count-1)
+                    {
+                        Console.WriteLine("Invalid index");
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    list.RemoveAt(num);
+                }
+                else if (inputs[1] == "left")
+                {
+                    int num = int.Parse(inputs[2]);
+                    for (int i = 0; i < num; i++)
+                    {
+                        list.Add(list[0]);
+                        list.RemoveAt(0);
+                    }
+                }
+                else if (inputs[1] == "right")
+                {
+                    int num = int.Parse(inputs[2]);
+                    for (int i = 0; i < num; i++)
+                    {
+                        list.Insert(0, list[list.Count - 1]);
+                        list.RemoveAt(list.Count - 1);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
---
+++ lists/listsExercise/Problem5/Problem5.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem5
+{
+    internal class Problem5
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> specials = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            //
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == specials[0])
+                {
+                    for (int j = 0; j < specials[1]; j++)
+                    {
+                        if (i < 1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            list.RemoveAt(i - 1);
+                            i--;
+                        }
+                    }
+                    for (int j = 0; j < specials[1]; j++)
+                    {
+                        if (i > list.Count - 2)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            list.RemoveAt(i + 1);
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == specials[0])
+                {
+                    list.RemoveAt(i);
+                    i--;
+                }
+            }
+            Console.WriteLine(list.Sum());
+        }
+    }
+}
