5a77f7a310e320daab3fba2bdb5a0820af178e10
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-07-09 23:31:23 +0300

    `

--- 6.ObjectsAndClasses/objectsAndClassesExercise/Problem1/Problem1.cs
+++ 6.ObjectsAndClasses/objectsAndClassesExercise/Problem1/Problem1.cs
@@ -1,30 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace arraysExercise
+namespace Problem1
 {
     internal class Problem1
     {
         static void Main(string[] args)
         {
-            int numWagons = int.Parse(Console.ReadLine());
-            int[] passangers = new int[numWagons];
-            int sum = 0;
-            for (int i = 0; i < numWagons; i++)
+            var phrases = "Excellent product./Such a great product./I always use that product./Best product of its category./Exceptional product./I can't live without this product."
+                .Split('/')
+                .ToArray();
+            var events = "Now I feel good./I have succeeded with this product./Makes miracles. I am happy of the results!/I cannot believe but now I feel awesome./Try it yourself, I am very satisfied./I feel great!"
+                .Split('/')
+                .ToArray();
+            var authors = "Diana/Petya/Stella/Elena/Katya/Iva/Annie/Eva"
+                .Split('/')
+                .ToArray();
+            var cities = "Burgas/Sofia/Plovdiv/Varna/Ruse"
+                .Split('/')
+                .ToArray();
+            //
+            int input = int.Parse(Console.ReadLine());
+            //
+            Random random= new Random();
+            //
+            for (int i = 1; i <= input; i++)
             {
-                passangers[i] = int.Parse(Console.ReadLine());
-                sum += passangers[i];
+                int index1 = random.Next(phrases.Length);
+                int index2 = random.Next(events.Length);
+                int index3 = random.Next(authors.Length);
+                int index4 = random.Next(cities.Length);
+                Console.WriteLine($"{phrases[index1]} {events[index2]} {authors[index3]} – {cities[index4]}.");
             }
-            foreach (int number in passangers)
-            {
-                Console.Write($"{number} ");
-            }
-            Console.WriteLine();
-            Console.WriteLine(sum);
         }
     }
 }
+
+    
+
+﻿
---
+++ 6.ObjectsAndClasses/objectsAndClassesExercise/Problem2/Problem2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.None)
+                .ToArray();
+            Article article = new Article(input1[0], input1[1], input1[2]);
+            int commandsCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= commandsCount; i++)
+            {
+                var input2 = Console.ReadLine()
+                .Split(new string[] { ": " }, StringSplitOptions.None)
+                .ToArray();
+                switch (input2[0])
+                {
+                    case "Edit":
+                        string newContent = input2[1];
+                        article.Edit(newContent);
+                        break;
+                    case "ChangeAuthor":
+                        string newAuthor = input2[1];
+                        article.ChangeAuthor(newAuthor);
+                        break;
+                    case "Rename":
+                        string newTitle = input2[1];
+                        article.Rename(newTitle);
+                        break;
+                }
+            }
+            Console.WriteLine(article.ToString());
+        }
+    }
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+        public void Rename(string newTitle)
+        {
+            Title = newTitle;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
