92fe2f676c1e0526b060a88aae3cf5ad23e67a63
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-07-09 20:23:11 +0300

    `

--- 6.ObjectsAndClasses/objectsAndClassesLecture/Problem5/Program.cs
+++ 6.ObjectsAndClasses/objectsAndClassesLecture/Problem5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,59 @@ namespace Problem4
     {
         static void Main(string[] args)
         {
+            var input = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+            List<Box> boxes = new List<Box>();
+            string name;
+            decimal price = 0;
+            string serialNumber;
+            int itemQuantity;
+            while (input[0] != "end")
+            {
+                name = input[1];
+                price = decimal.Parse(input[3]);
+                var item = new Item(name, price);
+                //
+                serialNumber = input[0];
+                itemQuantity = int.Parse(input[2]);
+                var box = new Box(serialNumber, itemQuantity, item);
+                boxes.Add(box);
+                //
+                input = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+            }
+            foreach (var box in boxes.OrderByDescending(x=>x.BoxPrice))
+            {
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.BoxPrice:f2}");
 
+            }
         }
+    }
+    public class Item
+    {
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class Box
+    {
+        public Box(string serialNumber, int itemQuantity, Item item)
+        {
+            SerialNumber = serialNumber;
+            ItemQuantity = itemQuantity;
+            Item = item;
+        }
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal BoxPrice => Item.Price * ItemQuantity;
     }
 }
---
+++ 6.ObjectsAndClasses/objectsAndClassesLecture/Problem7/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split('/')
+                .ToArray();
+            //
+            List<Car> cars= new List<Car>();
+            List<Truck> trucks= new List<Truck>();
+            //
+            string brand;
+            string model;
+            string weight;
+            string horsePower;
+            //
+            while (input[0] != "end")
+            {
+                if (input[0] == "Car")
+                {
+                    brand = input[1];
+                    model = input[2];
+                    horsePower= input[3];
+                    var car = new Car(brand, model, horsePower);
+                    cars.Add(car);
+                }
+                else
+                {
+                    brand= input[1];
+                    model = input[2];
+                    weight = input[3];
+                    var truck = new Truck(brand, model, weight);
+                    trucks.Add(truck);
+                }
+                input = Console.ReadLine()
+                .Split('/')
+                .ToArray();
+            }
+            Console.WriteLine("Cars:");
+            foreach (var car in cars.OrderBy(e => e.Brand))
+            {
+                Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+            }
+            Console.WriteLine("Trucks:");
+            foreach (var truck in trucks.OrderBy(e => e.Brand))
+            {
+                Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+            }
+        }
+    }
+    public class Truck 
+    {
+        public Truck(string brand, string model, string weight)
+        {
+            Brand = brand;
+            Model = model;
+            Weight = weight;
+        }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Weight { get; set; }
+    }
+    public class Car 
+    {
+        public Car (string brand, string model, string horsePower)
+        {
+            Brand = brand;
+            Model = model;
+            HorsePower = horsePower;
+        }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string HorsePower { get; set; }
+    }
+    public class VehicleCatalog
+    {
+        public VehicleCatalog(List<Car> cars, List<Truck> trucks)
+        {
+            Cars = cars;
+            Trucks = trucks;
+        }
+
+        public List<Car> Cars { get; set; }
+        public List<Truck> Trucks { get; set; }
+    }
+
+}
