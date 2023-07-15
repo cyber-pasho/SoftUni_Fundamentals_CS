22b37ea34e9afc3e1141fa8254d5b10c694615f0
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-07-15 22:39:07 +0300

    `

--- 6.ObjectsAndClasses/objectsAndClassesExercise/Problem3/Program.cs
+++ 6.ObjectsAndClasses/objectsAndClassesExercise/Problem3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,42 @@ namespace Problem3
     {
         static void Main(string[] args)
         {
+            int studentsCount = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+            for (int i = 1; i <= studentsCount; i++)
+            {
+                var studentInput = Console.ReadLine()
+                    .Split(' ')
+                    .ToArray();
+                string firstName = studentInput[0];
+                string lastName = studentInput[1];
+                double grade = double.Parse(studentInput[2]);
+                Student student = new Student(firstName,lastName,grade);
+                students.Add(student);
+            }
+            List<Student> sortedStudents = SortStudentsByGradeDescending(students);
+            foreach (var entry in sortedStudents)
+            {
+                Console.WriteLine($"{entry.FirstName} {entry.LastName}: {entry.Grade:f2}");
+            }
         }
+        public static List<Student> SortStudentsByGradeDescending(List<Student> students)
+        {
+            students.Sort((s1, s2) => s2.Grade.CompareTo(s1.Grade));
+            return students;
+        }
+    }
+
+    public class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
     }
 }
