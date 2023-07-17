9cbe8ec98bba6e8539f667124af602e6ae69fe75
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-07-17 23:50:45 +0300

    `

---
+++ 6.ObjectsAndClasses/objectsAndClassesMoreExercise/Problem1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
