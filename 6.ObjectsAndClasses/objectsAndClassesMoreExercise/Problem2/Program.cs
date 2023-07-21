879485cc855c9425a57f8babc5342c183202c716
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-07-21 17:45:57 +0300

    `

---
+++ 6.ObjectsAndClasses/objectsAndClassesMoreExercise/Problem2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int familyCoult = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < familyCoult; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ")
+                    .ToArray();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Console.WriteLine($"{ family.GetOldestMember().Name} {family.GetOldestMember().Age}");
+        }
+    }
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+            People.Add(person);
+        }
+        public Person GetOldestMember()
+        {
+            return People.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+        public List<Person> People { get; set; } 
+    }
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name  { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ 6.ObjectsAndClasses/objectsAndClassesMoreExercise/Problem3/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Problem3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int carsCount = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < carsCount; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ")
+                    .ToArray();
+                string model = input[0];
+                double fuel = double.Parse(input[1]);
+                double fuelPerKM = double.Parse(input[2]);
+                Car car = new Car(model,fuel,fuelPerKM);
+                cars.Add(car);
+            }
+            string statement;
+            while ((statement = Console.ReadLine()) != "End" && !string.IsNullOrWhiteSpace(statement))
+            {
+                var action = statement
+                    .Split(" ")
+                    .ToArray();
+                string operation = action[0];
+                string model = action[1];
+                int distance = int.Parse(action[2]);
+                var currentCar = cars.FirstOrDefault(c => c.Model == model);
+                currentCar?
+                    .CanTravel(distance);
+            }
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+    public class Car
+    {
+        public Car(string model, double fuel, double fuelPerKM)
+        {
+            Model = model;
+            Fuel = fuel;
+            FuelPerKM = fuelPerKM;
+            Distance = 0;
+        }
+        public bool CanTravel(int distance)
+        {
+            double fuelConsumed = distance * FuelPerKM;
+            if (fuelConsumed >= Fuel)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+                return false;
+            }
+            else
+            {
+                Fuel -= fuelConsumed;
+                Distance += distance;
+                return true;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Model} {Fuel:f2} {Distance}";
+        }
+
+        public string Model { get; set; }
+        public double Fuel { get; set; }
+        public double FuelPerKM { get; set; }
+        public int Distance { get; set; }
+    }
+}
