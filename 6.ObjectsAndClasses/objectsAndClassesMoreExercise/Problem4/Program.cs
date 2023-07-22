1232e1faa00b69d3d4920b46d77a7595242c4fcf
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-07-22 16:50:44 +0300

    `

--- 6.ObjectsAndClasses/objectsAndClassesMoreExercise/Problem4/Program.cs
+++ 6.ObjectsAndClasses/objectsAndClassesMoreExercise/Problem4/Program.cs
@@ -4,7 +4,85 @@
     {
         static void Main()
         {
-            
+            int carsCount = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < carsCount; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ")
+                    .ToList();
+                string model = input[0];
+                int engineSpeed = int.Parse(input[1]);
+                int enginePower = int.Parse(input[2]);
+                int cargoWeight = int.Parse(input[3]);
+                string cargoType = input[4];
+                Engine engine = new Engine(engineSpeed, enginePower);
+                Cargo cargo = new Cargo(cargoWeight, cargoType);
+                Car car = new Car(model,engine,cargo);
+                cars.Add(car);
+
+            }
+            string type = Console.ReadLine();
+            foreach (var car in cars)
+            {
+                car.TypeAction(type);
+            }
         }
+    }
+    public class Car
+    {
+        public Car(string model, Engine engine, Cargo cargo)
+        {
+            Model = model;
+            Engine = engine;
+            Cargo = cargo;
+        }
+        public void TypeAction(string type)
+        {
+            switch (type)
+            {
+                case "fragile":
+                    if (Cargo.CargoWeight < 1000 && Cargo.CargoType == "fragile")
+                    {
+                        Console.WriteLine($"{Model}");
+                    }
+                    break;
+                case "flamable":
+                    if (Engine.EnginePower > 250 && Cargo.CargoType == "flamable")
+                    {
+                        Console.WriteLine($"{Model}");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+    }
+    public class  Engine
+    {
+        public Engine(int engineSpeed, int enginePower)
+        {
+            EngineSpeed = engineSpeed;
+            EnginePower = enginePower;
+        }
+
+        public int EngineSpeed { get; set; }
+        public int EnginePower { get; set; }
+
+    }
+    public class Cargo
+    {
+        public Cargo(int cargoWeight, string cargoType)
+        {
+            CargoWeight = cargoWeight;
+            CargoType = cargoType;
+        }
+
+        public int CargoWeight { get; set; }
+        public string CargoType { get; set; }
     }
 }
---
+++ 6.ObjectsAndClasses/objectsAndClassesMoreExercise/Problem5/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Reflection;
+
+namespace Problem5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
+            //people input start
+            var peopleInput = Console.ReadLine()
+                .Split(";")
+                .Where(item => !string.IsNullOrWhiteSpace(item))
+                .ToList();
+            people = peopleInput
+                .Select(s => s.Split('='))
+                .Select(parts => new Person(parts[0], decimal.Parse(parts[1])))
+                .ToList();
+            //people input end
+            
+            //products input start
+            var productsInput = Console.ReadLine()
+                .Split(";")
+                .Where(item => !string.IsNullOrWhiteSpace(item))
+                .ToList();
+            products = productsInput
+                .Select(s => s.Split('='))
+                .Select(parts => new Product(parts[0], decimal.Parse(parts[1])))
+                .ToList();
+            //products input end
+
+            // EXECUTION START//
+            string command;
+            string personName;
+            string productName;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                List<string> commandList = command
+                    .Split(" ")
+                    .ToList();
+                personName = commandList[0];
+                productName = commandList[1];
+                var person = people.FirstOrDefault(p => p.Name == personName);
+                if (person != null)
+                {
+                    person.OutputMassage(products.FirstOrDefault(p => p?.Name == productName));
+                }
+            }
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+            // EXECUTION END//
+        }
+    }
+    public class Person
+    {
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            Products = new List<Product>();
+        }
+        public void OutputMassage(Product product)
+        {
+            if (Money >= product.Cost)
+            {
+                Money -= product.Cost;
+                Products.Add(product);
+                Console.WriteLine($"{Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} can't afford {product.Name}");
+            }
+        }
+        public override string ToString()
+        {
+            if (Products.Count != 0)
+            {
+                return $"{Name} - {string.Join(", ", Products)}";
+            }
+            else
+            {
+                return $"{Name} - Nothing bought";
+            }
+        }
+
+        public string Name { get; set; }
+        public decimal Money { get; set; }
+        public List<Product> Products { get; set; }
+    }
+    public class Product
+    {
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+    }
+    
+}
