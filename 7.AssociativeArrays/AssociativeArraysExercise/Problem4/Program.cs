0879a44153a81d5edad81dd3a6baa16e640aea88
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-08-02 23:52:07 +0300

    `

---
+++ 7.AssociativeArrays/AssociativeArraysExercise/Problem4/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Problem4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numberOperations = int.Parse(Console.ReadLine());
+            Dictionary<string, string> users = new Dictionary<string, string>();
+            string licensePlateNumber = string.Empty;
+            for (int i = 0; i < numberOperations; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(" ")
+                    .ToList();
+                string command = input[0];
+                string username = input[1];
+                if (command != "unregister")
+                {
+                    licensePlateNumber = input[2];
+                }
+                Operation(command, username, licensePlateNumber, users);
+            }
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
+        }
+        public static void Operation (string command, string username, string licensePlateNumber, Dictionary<string, string> users)
+        {
+
+            if (command == "register")
+            {
+                if (!users.ContainsKey(username))
+                {
+                    users.Add(username, licensePlateNumber);
+                    Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");  
+                }
+            }
+            else
+            {
+                if (!users.ContainsKey(username))
+                {
+                    Console.WriteLine($"ERROR: user {username} not found");
+                }
+                else
+                {
+                    users.Remove(username);
+                    Console.WriteLine($"{username} unregistered successfully");
+                }
+                
+            }
+        }
+    }
+}
---
+++ 7.AssociativeArrays/AssociativeArraysExercise/Problem5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Problem5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int studentsCount = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            for (int i = 0; i < studentsCount; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if (!students.ContainsKey(studentName))
+                {
+                    students.Add(studentName, new List<double>());
+                    students[studentName].Add(grade);
+                }
+                else
+                {
+                    students[studentName].Add(grade);
+                }
+            }
+            var successfulStudents = students
+                .Where(s => s.Value.Average() >= 4.50)
+                .ToList();
+            foreach (var student in successfulStudents)
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+            }
+        }
+    }
+}
