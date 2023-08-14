b0aa1fa537b08b56eba20c9ac3b842f994811163
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-08-14 22:53:07 +0300

    1

--- 7.AssociativeArrays/AssociativeArraysExercise/Problem5/Program.cs
+++ 7.AssociativeArrays/AssociativeArraysExercise/Problem5/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Problem5
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Problem5
 {
     internal class Program
     {
         static void Main()
         {
-            
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] inputs = input
+                    .Split(" : ");
+                List<string> students = new List<string>();
+                string courseName = inputs[0];
+                string studentName = inputs[1];
+                if (!courses.ContainsKey(courseName))
+                {
+                    students.Add(studentName);
+                    courses.Add(courseName, students);
+                }
+                else 
+                {
+                    students.Add(studentName);
+                }
+            }
+            foreach (var item in courses)
+            {
+                Console.WriteLine($"{item}: {item.Value.Count}");
+                foreach (var name in item.Value)
+                {
+                    Console.WriteLine($"{}");
+                }
+            }
         }
     }
 }
