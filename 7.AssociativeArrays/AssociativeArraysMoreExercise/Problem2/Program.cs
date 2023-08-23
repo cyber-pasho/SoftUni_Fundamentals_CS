a3aaae089333eb949808e1a85ed51da0166712c0
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-08-23 10:33:08 +0300

    `

---
+++ 7.AssociativeArrays/AssociativeArraysMoreExercise/Problem2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Problem2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input;
+            Dictionary<string, int> people = new Dictionary<string, int>();
+            List<Contest> contests = new List<Contest>();
+            while ((input = Console.ReadLine()) != "no more time")
+            {
+                var statement = input
+                    .Split(" -> ")
+                    .ToList();
+                string nameUser = statement[0];
+                string nameContest = statement[1];
+                int points = int.Parse(statement[2]);
+                //
+                if (!contests.Any(x=>x.Name == nameContest))
+                {
+                    contests.Add(new Contest(nameContest));  
+                }
+                
+            }
+        }
+    }
+    public class Contest
+    {
+        public Contest(string name)
+        {
+            Name = name;
+            Person = new List<Dictionary<string, int>>();
+        }
+
+        public string Name { get; set; }
+
+        public List<Dictionary<string, int>> Person { get; set; }
+    }
+}
