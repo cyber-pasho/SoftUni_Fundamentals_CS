e7357fed94fa9a2b3b25cce4210b9040ba74b48a
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-08-29 02:17:43 +0300

    `

---
+++ 7.AssociativeArrays/AssociativeArraysMoreExercise/Problem3.1/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Problem3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> players = new Dictionary<string, Dictionary<string, int>>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "Season end")
+            {
+                var statement = input.Split(" ").ToList();
+
+                if (statement.Count < 5)
+                {
+                    string movement = string.Join("", statement);
+                    var action = movement.Split("vs").ToList();
+                    string dueler1 = action[0];
+                    string dueler2 = action[1];
+
+                    if (players.ContainsKey(dueler1) && players.ContainsKey(dueler2))
+                    {
+                        foreach (var pos1 in players[dueler1])
+                        {
+                            foreach (var pos2 in players[dueler2])
+                            {
+                                if (pos1.Key == pos2.Key)
+                                {
+                                    int player1TotalPoints = players[dueler1].Values.Sum();
+                                    int player2TotalPoints = players[dueler2].Values.Sum();
+                                    if (player1TotalPoints > player2TotalPoints)
+                                    {
+                                        players.Remove(dueler2);
+                                    }
+                                    else if (player2TotalPoints > player1TotalPoints)
+                                    {
+                                        players.Remove(dueler1);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    string movement = string.Join("", statement);
+                    var action = movement.Split("->").ToList();
+                    string playerName = action[0];
+                    string playerPosition = action[1];
+                    int skill = int.Parse(action[2]);
+
+                    if (!players.ContainsKey(playerName))
+                    {
+                        players.Add(playerName, new Dictionary<string, int>());
+                        players[playerName].Add(playerPosition, skill);
+                    }
+                    else
+                    {
+                        if (!players[playerName].ContainsKey(playerPosition))
+                        {
+                            players[playerName].Add(playerPosition, skill);
+                        }
+                        else
+                        {
+                            if (players[playerName][playerPosition] < skill)
+                            {
+                                players[playerName][playerPosition] = skill;
+                            }
+                        }
+                    }
+                }
+            }
+            //PRINTS
+            int playerTotalPoints = 0;
+            Dictionary<int, string> bestPlayers = new Dictionary<int, string>();
+            foreach (var player in players)
+            {
+                playerTotalPoints = player.Value
+                    .Values
+                    .Sum();
+                bestPlayers.Add(playerTotalPoints, player.Key);
+            }
+            var sortedPlayers = bestPlayers
+                .OrderBy(p => p.Value)
+                .OrderByDescending(p => p.Key);
+
+            foreach (var sortedPlayer in sortedPlayers)
+            {
+                Console.WriteLine($"{sortedPlayer.Value}: {sortedPlayer.Key} skill");
+                foreach (var player in players)
+                {
+                    if (player.Key == sortedPlayer.Value)
+                    {
+                       var sortedPositions = player.Value
+                           .OrderBy(p => p.Key)
+                           .OrderByDescending(p => p.Value);
+                       foreach (var sortedPosition in sortedPositions)
+                       {
+                           Console.WriteLine($"- {sortedPosition.Key} <::> {sortedPosition.Value}");
+                       }
+                    }
+                }
+            }
+        }
+    }
+}
