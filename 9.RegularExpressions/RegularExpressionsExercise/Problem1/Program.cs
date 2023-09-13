8f556ba6c8d4bf5c48483c0c275e1444350edd30
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-09-14 00:56:24 +0300

    `

--- 9.RegularExpressions/RegularExpressionsExercise/Problem1/Program.cs
+++ 9.RegularExpressions/RegularExpressionsExercise/Problem1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace Problem1
 {
@@ -6,7 +7,29 @@ namespace Problem1
     {
         static void Main()
         {
-            
+            string pattern = @">>(?<Product>[A-Za-z]+)<<(?<Price>[0-9]+|[0-9]+.[0-9]+)!(?<Quantity>[0-9]+)";
+            string input;
+            decimal totalPrice = 0;
+            List<string> products = new List<string>();
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                Regex regex = new Regex(pattern);
+                MatchCollection matches = Regex.Matches(input, pattern);
+                foreach (Match match in matches)
+                {
+                    string product = match.Groups["Product"].Value;
+                    decimal cost = decimal.Parse(match.Groups["Price"].Value, CultureInfo.InvariantCulture);
+                    decimal price = cost * int.Parse(match.Groups["Quantity"].Value);
+                    products.Add(product);
+                    totalPrice += price;
+                }
+            }
+            Console.WriteLine($"Bought furniture:");
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total money spend: {totalPrice:f2}");
         }
     }
 }
---
+++ 9.RegularExpressions/RegularExpressionsExercise/Problem2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Problem2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var participants = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+            string patternName = @"[A-Za-z]+";
+            string patternDistance = @"[0-9]";
+        }
+    }
+}
