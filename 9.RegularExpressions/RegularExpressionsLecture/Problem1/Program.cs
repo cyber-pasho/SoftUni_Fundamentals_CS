a697eff036fc814a63a67a3d83102943ceaa9a71
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-09-14 00:13:05 +0300

    `

---
+++ 9.RegularExpressions/RegularExpressionsLecture/Problem1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<FirstName>\b[A-Z][a-z]+)[ ](?<LastName>\b[A-Z][a-z]+)";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match name in matches)
+            {
+                Console.Write($"{name.Value} ");
+            }
+        }
+    }
+}
---
+++ 9.RegularExpressions/RegularExpressionsLecture/Problem3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Problem3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<day>\b[0-9]{2}\b)(\.|-|\/)(?<Month>[A-Z][a-z]{2})\1(?<Year>[0-9]{4})";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["Month"]}, Year: {match.Groups["Year"]}");
+            }
+        }
+    }
+}
