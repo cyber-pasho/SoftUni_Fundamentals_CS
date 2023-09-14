e8a5f8a2b13e812fe67d666935865481872a468b
Stoycho Sirakov <stoycho.sirakov@gmail.com>
2023-09-14 23:43:18 +0300

    `

---
+++ 9.RegularExpressions/RegularExpressionsExercise/Problem3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Problem3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string pattern = @"%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>\d+\.\d+|\d+)\$";
+            Regex regex = new Regex(pattern);
+            string input;
+            decimal totalMoney = 0;
+            while ((input = Console.ReadLine())!= "end of shift")
+            {
+                MatchCollection matches = Regex.Matches(input, pattern);
+                foreach (Match match in matches)
+                {
+                    int count = int.Parse(match.Groups["count"].Value);
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+                    decimal money = count*price;
+                    totalMoney += money;
+                    Console.WriteLine($"{match.Groups["customer"].Value}: {match.Groups["product"].Value} - {money}");
+                }
+                Console.WriteLine($"Total income: {totalMoney}");
+            }
+        }
+    }
+    
+}
